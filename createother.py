79a1b0da17c3b8d8ae3b1da3764d96807f0e9985
Michael Hearne <mhearne@usgs.gov>
2013-07-10 12:48:10 -0600

    Fixed the fix to table create

--- createother.py
+++ createother.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
                    'magnitude':'float','time':'datetime'}
 
     print 'Creating %s...' % tabledef
-    query = 'CREATE TABLE %other ('
+    query = 'CREATE TABLE other ('
     for cname,cvalue in tabledef.iteritems():
         query = query + ' %s %s,' % (cname,cvalue)
         query = query[0:-1] + ')'
