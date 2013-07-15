2e0b872ce7b5380420fb3ba5d25b5cac603d072a
Michael Hearne <mhearne@usgs.gov>
2013-07-15 14:47:48 -0600

    Added option to skip over bad station files

--- atlas2db.py
+++ atlas2db.py
@@ -716,7 +716,7 @@ if __name__ == '__main__':
         print 'Saving event %s' % f
         pusher.pushEvent(eventfile,configfiles,stationfiles,sourcefile,faultfile,runfile,
                          statusfile,options.shakehome,doUpdate,version,revision,
-                         dbdict['shakemap'],skipBadStations=skipBadStations)
+                         dbdict['shakemap'],skipBadStations=options.skipBadStations)
         
         
     pusher.close()
