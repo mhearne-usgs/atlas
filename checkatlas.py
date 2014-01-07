7a7f8398b877f7985c2fcbdf791340e0e47092b6
Michael Hearne <mhearne@usgs.gov>
2014-01-07 16:30:41 -0700

    adding constrained info

--- checkatlas.py
+++ checkatlas.py
@@ -158,10 +158,10 @@ def printEventErrors(eventcode,folder,eventxml,faultdict):
 
     if isConstrained:
         if fault1bad or fault3bad or fault5bad:
-            print 'Event %s issues: %s' % (eventcode,' | '.join(nuggets))
+            print 'Event %s (constrained) issues: %s' % (eventcode,' | '.join(nuggets))
     else:
         if fault1bad or fault2bad or fault3bad or fault4bad or fault5bad or mechbad:
-            print 'Event %s issues: %s' % (eventcode,'|'.join(nuggets))
+            print 'Event %s (reviewed) issues: %s' % (eventcode,'|'.join(nuggets))
     
         
 
