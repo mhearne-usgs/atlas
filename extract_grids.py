98fb3f673d04e5b0d3519aa82af2f49948f5217c
Michael Hearne <mhearne@usgs.gov>
2014-04-09 08:57:46 -0600

    initial check-in for extract_grids script

---
+++ extract_grids.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import zipfile
+import os.path
+import sys
+
+if __name__ == '__main__':
+    rootfolder = sys.argv[1]
+    if rootfolder.endswith(os.sep):
+        rootfolder = rootfolder[0:-1]
+    p,f = os.path.split(rootfolder)
+    zipname = '%s_grids.zip' % (f)
+    myzip = zipfile.ZipFile(zipname,zipfile.ZIP_DEFLATED)
+    folders = os.listdir(rootfolder)
+    for tfolder in folders:
+        folder = os.path.join(rootfolder,tfolder)
+        gridfile = os.path.join(folder,'output','grid.xml')
+        errorfile = os.path.join(folder,'output','uncertainty.xml')
+        gridarcname = '%s_grid.xml' % tfolder
+        errorarcname = '%s_error.xml' % tfolder
+        myzip.write(gridarcname,gridfile)
+        myzip.write(errorarcname,errorfile)
+    myzip.close()
